b06ce89fbc6cb4cd956ebbac1d040a8b499a0859
Peeush Agarwal <agarwal.peeush@gmail.com>
2019-09-24 23:11:30 +0530

    TaxiFare prediction ModelBuilder demo

---
+++ TaxiFarePrediction/TaxiFarePrediction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TaxiFarePredictionML.Model;
+
+namespace TaxiFarePrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumeModel();
+            Console.ReadKey();
+        }
+
+        static void ConsumeModel()
+        {
+            // Add input data
+            var input = new ModelInput()
+            {
+                Rate_code = 1,
+                Passenger_count = 1,
+                Trip_time_in_secs = 821,
+                Trip_distance = 2.5f,
+                Payment_type = "CSH",
+                Vendor_id = "CMT",
+            };
+
+            // Load model and predict output of sample data
+            ModelOutput result = TaxiFarePredictionML.Model.ConsumeModel.Predict(input);
+            Console.WriteLine($"Taxi Fare predicted = {result.Score}");
+        }
+    }
+}
