ca650af45c70c62a2ebf9b9236195e95ba1aaf6d
Peeush Agarwal <agarwal.peeush@gmail.com>
2019-09-24 21:50:08 +0530

    First ML Model using SentimentAnalysis from ModelBuilder extension.

---
+++ FirstMLApp/FirstMLApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using FirstMLAppML.Model;
+using Microsoft.ML;
+using System;
+
+namespace FirstMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumeModel();
+            Console.ReadKey();
+        }
+
+        static void ConsumeModel()
+        {
+            // Load the model
+            MLContext mlContext = new MLContext();
+
+            ITransformer mlModel = mlContext.Model.Load("MLModel.zip", out var modelInputSchema);
+
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            // Use the code below to add input data
+            var input = new ModelInput();
+            input.SentimentText = "You're so rude";
+
+            // Try model on sample data
+            // True is toxic, false is non-toxic
+            ModelOutput result = predEngine.Predict(input);
+
+            Console.WriteLine($"Text: {input.SentimentText} | Prediction: {(Convert.ToBoolean(result.Prediction) ? "Toxic" : "Non Toxic")} sentiment");
+        }
+    }
+}
